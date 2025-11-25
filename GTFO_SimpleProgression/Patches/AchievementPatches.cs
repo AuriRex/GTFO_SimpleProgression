779f67acf9be3ac0d2895c6fb311a98f79ca006e
AuriRex <xaurirexx@gmail.com>
2025-11-25 21:10:01 +0100

    Extra ReadAllLogs check for modded

--- GTFO_SimpleProgression/Patches/AchievementPatches.cs
+++ GTFO_SimpleProgression/Patches/AchievementPatches.cs
@@ -7,6 +7,12 @@ public static class Achievement_ReadAllLogs__CheckCompletion__Patch
 {
     public static bool Prefix(Achievement_ReadAllLogs __instance)
     {
+        if (__instance.m_allLogs.Count != Achievement_ReadAllLogs.TOTAL_LOGS_FOR_ACHIEVEMENT)
+        {
+            // We skip in case we aren't running vanilla log counts ig
+            return false;
+        }
+        
         if (SteamManager.Current.GetAchievement(__instance.AchievementKey))
         {
             // ReadAllLogs is unlocked, so we don't need to do anything and cancel OG
